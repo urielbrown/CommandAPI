fe328f46735a770eae4d3edc6fc17f996f43f651
= <amburb@gmail.com>
2021-02-08 20:00:24 -0500

    Added the Database Related Classes

---
+++ src/CommandApi/Data/SqlCommandAPIRepo.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using CommandApi.Models;
+using System.Linq;
+
+namespace  CommandApi.Data
+{
+    public class SqlCommandAPIRepo : ICommandAPIRepo
+    {
+        private readonly CommandContext _context;
+        public SqlCommandAPIRepo(CommandContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _context.CommandItems.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _context.CommandItems.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+    }    
+}
